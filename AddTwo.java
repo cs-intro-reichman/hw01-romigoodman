72cc50c92afd80a280e1b8af40eb27abcb17b7c5
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-21 22:41:53 +0200

    AddTwo Ex1

--- AddTwo.java
+++ AddTwo.java
@@ -4,6 +4,11 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		// Put your code here	
-		//change to test
+		int a = 10;
+		int b = 14;
+		int sumOfNumbers = 24;
+
+		System.out.println("% java AddTwo 10 14");
+		System.out.println("10 + 14 = 24");
 	}
 }
