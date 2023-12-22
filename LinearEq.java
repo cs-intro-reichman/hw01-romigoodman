04cb251e45409178efc14188c1e727615afea7fc
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-22 17:10:07 +0200

    final

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,21 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+	public static void main(String[] args) {
+
+	// defining a, b, c
+	int numberA;
+	int numberB;
+	int numberC;
+	numberA = Integer.parseInt(args[0]);
+	numberB = Integer.parseInt(args[1]);
+	numberC = Integer.parseInt(args[2]);
+
+
+	int x = (numberC - numberB) / numberA;
+
+	System.out.println(numberA + " * x + " + numberB + " = " + numberC);
+	System.out.println("x = " + x);
+
 }
+	}
