79c3598a3911f49c50cdf1e425bca879b79ae8e3
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-25 11:24:31 +0200

    try

--- GenThree.java
+++ GenThree.java
@@ -6,9 +6,9 @@ public class GenThree {
 	int min = Integer.parseInt(args[0]);
 	int max = Integer.parseInt(args[1]);
 	
-	int randomNumber1 =(int)((Math.random() *max-min+1)+min);
-	int randomNumber2 =(int)((Math.random() *max-min+1)+min);
-	int randomNumber3 =(int)((Math.random() *max-min+1)+min);
+	int randomNumber1 =(int)(Math.random() * (max-min+1) +min);
+	int randomNumber2 =(int)(Math.random() * (max-min+1) +min);
+	int randomNumber3 =(int)(Math.random() * (max-min+1) +min);
 
 	System.out.println (randomNumber1);
 	System.out.println (randomNumber2);
